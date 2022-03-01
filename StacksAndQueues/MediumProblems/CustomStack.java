c778c3e22018260f928d08b6a1b9e7f1c3bfcd73
maram gowthami <67852967+gowthami123-sudo542@users.noreply.github.com>
2022-03-01 15:09:31 +0530

    Update CustomStack.java

--- StacksAndQueues/MediumProblems/CustomStack.java
+++ StacksAndQueues/MediumProblems/CustomStack.java
@@ -1,4 +1,4 @@
-
+// By using the list
 class CustomStack {
     private List<Integer> list=new ArrayList<>();
     private int size;
@@ -26,4 +26,33 @@ class CustomStack {
         }
     }
 }
+// By using Array
+class CustomStack
+{
+    int[] stack;
+    int size;
+    int index;
+    public CustomStack(int maxSize) {
+        stack = new int[maxSize];
+        size = maxSize;
+        index=0;
+    }
 
+    public void push(int x) {
+       if (index<size){
+            stack[index++] = x;
+        }
+    }
+
+    public int pop() {
+        if(index == 0) return -1;
+        return stack[--index];
+    }
+
+    public void increment(int k, int val) {
+        if(stack.length == 0) return;
+        for(int i = 0;i<k && i<stack.length;i++)
+            stack[i]= stack[i] + val;
+    }
+            
+}
