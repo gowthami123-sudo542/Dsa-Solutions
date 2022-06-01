5290de2745bcc7a8ba219494d656452daf968aff
maram gowthami <67852967+gowthami123-sudo542@users.noreply.github.com>
2022-06-01 09:19:31 +0530

    Create RunningSum.java

---
+++ LeetDailyProblems/June/RunningSum.java
@@ -0,0 +1,16 @@
+## Time Complexity:-O(n)
+  class Solution {
+    public int[] runningSum(int[] nums) 
+    {
+        int arr[]=new int[nums.length];
+        int sum=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            sum=sum+nums[i];
+            arr[i]=sum;
+            
+        }
+        return arr;
+        
+    }
+}
