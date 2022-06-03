053d9c75056decec6ee314264e2d706e3df56d06
maram gowthami <67852967+gowthami123-sudo542@users.noreply.github.com>
2022-06-03 11:16:30 +0530

    Update RangeSumQuery.java

--- LeetDailyProblems/June/RangeSumQuery.java
+++ LeetDailyProblems/June/RangeSumQuery.java
@@ -1,3 +1,4 @@
+## Time Complexity:-O(n^2)
 private int [][] data;
       // Constructor
     public NumMatrix(int[][] matrix) 
@@ -23,4 +24,28 @@ private int [][] data;
       }
         return sum;
         
+    
+##Time Complexity:-O(m*n)
+## By using Dp
+public class NumMatrix {
+private int[][] dp;
+    public NumMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                int left = (j == 0) ? 0 : matrix[i][j - 1];
+                int up = (i == 0) ? 0 : matrix[i - 1][j];
+                int corner = (i == 0 || j == 0) ? 0 : matrix[i - 1][j - 1];
+                matrix[i][j] += (left + up - corner);
+            }
+        }
+        dp = matrix;
     }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        int left = (row1 > 0) ? dp[row1 - 1][col2] : 0;
+        int up = (col1 > 0) ? dp[row2][col1 - 1] : 0;
+        int corner = (row1 > 0 && col1 > 0) ? dp[row1 - 1][col1 - 1] : 0;
+        return dp[row2][col2] - left - up + corner;
+    }
+}
+ 
