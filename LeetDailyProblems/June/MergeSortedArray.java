82b28b935301a6c29dfb5e4dcf45832d9321e82e
maram gowthami <67852967+gowthami123-sudo542@users.noreply.github.com>
2022-06-07 17:18:49 +0530

    Create MergeSortedArray.java

---
+++ LeetDailyProblems/June/MergeSortedArray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) 
+    {
+        int i=m-1;
+        int j=n-1;
+        int total=m+n-1;
+        while(i>=0 && j>=0)
+        {
+            nums1[total--]=(nums1[i]>nums2[j])?nums1[i--]:nums2[j--];
+        }
+        while(j>=0)
+        {
+            nums1[total--]=nums2[j--];
+        }
+        
+    }
+}
